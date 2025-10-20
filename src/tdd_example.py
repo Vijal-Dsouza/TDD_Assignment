cfd0357671595ff509a8c2f7a4ed9b05e88fb73d
VijalDsouza <vijal.datagrokr.co>
2025-10-20 16:05:54 +0530

    initial commit

--- src/tdd_example.py
+++ src/tdd_example.py
@@ -6,23 +6,30 @@ class TDDExample():
         """
         Reverses order of characters in string input_str.
         """
-        return None
+        return input_str[::-1]
 
     def find_longest_word(self, sentence: str) -> str:
         """
         Returns the longest word in string sentence.
         In case there are several, return the first.
         """
-        return None
+        words = sentence.split(" ")
+        longest_word = max(words, key=len)
+        return longest_word
 
     def reverse_list(self, input_list: list) -> list:
         """
         Reverses order of elements in list input_list.
         """
-        return []
+        res =[]
+        for i in range(len(input_list)-1, -1, -1):
+            res.append(input_list[i])
+        return res
 
     def count_digits(self, input_list: list, number_to_be_counted: int) -> int:
         """
         Return count of digits
         """
-        return 0
+
+        return len(set(input_list))
+    
